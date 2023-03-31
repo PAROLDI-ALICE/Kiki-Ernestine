b6f8cd3f3450af92ad0a34341e7d659037f22e90
PAROLDI-ALICE <aliceparoldi@gmail.com>
2023-03-31 10:01:50 +0200

    Update index route

--- resources/views/layout/general.blade.php
+++ resources/views/layout/general.blade.php
@@ -11,32 +11,32 @@
 
 <body>
     @guest
-        <div class="navBar">
-            <div class="left">
-                <img src="{{ asset('logo.png') }}" class="logo" />
-                <h1 class="bigt">La Rotonde</h1>
-            </div>
-            <div class="right">
-                <button><a class="button-link" href="home">Salon</a></button>
-                <button><a class="button-link" href="inscription">Inscription</a></button>
-                <button><a class="button-link" href="connexion">Connexion</a></button>
-            </div>
+    <div class="navBar">
+        <div class="left">
+            <img src="{{ asset('logo.png') }}" class="logo" />
+            <h1 class="bigt">La Rotonde</h1>
         </div>
+        <div class="right">
+            <button><a class="button-link" href="{{ route('index') }}">Salon</a></button>
+            <button><a class="button-link" href="inscription">Inscription</a></button>
+            <button><a class="button-link" href="connexion">Connexion</a></button>
+        </div>
+    </div>
     @endguest
     @auth
-        <div class="navBar">
-            <div class="left">
-                <img src="{{ asset('logo.png') }}" alt="logo" class="logo" />
-                <h1 class="bigt">La Rotonde</h1>
-            </div>
-            <div class="right">
-                <button><a class="button-link" href="home">Salon</a></button>
-                {{-- lister les personnages + creation nouveau --}}
-                <button><a class="button-link" href="personnages">Personnages</a></button>
-                <button><a class="button-link" href="groupes">Groupes</a></button>
-                <button><a class="button-link" href="deconnexion">Déconnexion</a></button>
-            </div>
+    <div class="navBar">
+        <div class="left">
+            <img src="{{ asset('logo.png') }}" alt="logo" class="logo" />
+            <h1 class="bigt">La Rotonde</h1>
         </div>
+        <div class="right">
+            <button><a class="button-link" href="home">Salon</a></button>
+            {{-- lister les personnages + creation nouveau --}}
+            <button><a class="button-link" href="personnages">Personnages</a></button>
+            <button><a class="button-link" href="groupes">Groupes</a></button>
+            <button><a class="button-link" href="deconnexion">Déconnexion</a></button>
+        </div>
+    </div>
     @endauth
 
 
