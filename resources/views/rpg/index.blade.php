47f73f17750d04c5a69a057bb734546bc7af0416
Systolicious <123855559+Systolicious@users.noreply.github.com>
2023-03-31 09:02:06 +0200

    styleChanges

--- resources/views/rpg/index.blade.php
+++ resources/views/rpg/index.blade.php
@@ -4,27 +4,28 @@
     <div class="article">
         <img src="{{ asset('kiki.jpg') }}" alt="kiki" class="kiki" />
         <div class="text">
-            <h2>Kiki de Montparnasse</h2>
+            <h2>Kiki de Montparnasse</h2><br />
             <p>Dans le Montparnasse de bohème et de génie des années vingt, Kiki réussit à s'extraire de la misère pour
                 devenir
-                l'une des figures les plus charismatiques de l'avant-garde de l'entre-deux-guerres. </p>
+                l'une des figures les plus charismatiques de l'avant-garde de l'entre-deux-guerres. </p><br />
 
             <p>Kiki aime la vie, aime les arts, les hommes, l’alcool et la fête… En 1929, elle passe 10 jours en prison à
                 Nice
                 après
-                s’en être violemment pris à un patron de bar qui l’avait recalée...</p>
+                s’en être violemment pris à un patron de bar qui l’avait recalée...</p><br />
             <p>Kiki était également une amoureuse des animaux et avait plusieurs chats. Elle a un jour sauvé un chat des
                 griffes
                 d'un chien en se battant avec l'animal.
-            </p>
+            </p><br />
             <p>Kiki était également connue pour son appétit et sa gourmandise. Elle aurait mangé une fois un kilo de fraises
                 en
                 une
                 seule soirée, et aurait été surprise en train de voler des croissants dans une boulangerie à Paris.</p>
+            <br />
             <p>Kiki était très superstitieuse et croyait aux fantômes. Elle aurait même emmené des amis visiter des
                 cimetières
                 la
-                nuit pour essayer de communiquer avec les morts.</p>
+                nuit pour essayer de communiquer avec les morts.</p><br /><br />
             <h2>Choisissez votre personnage de la vie de Kiki puis créez son cercle social dans le menu.</h2>
         </div>
     </div>
