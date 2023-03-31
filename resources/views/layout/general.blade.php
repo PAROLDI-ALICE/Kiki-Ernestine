8dde7a5f7e7c6a0b1df81be9b83f759d8dc215f7
Systolicious <123855559+Systolicious@users.noreply.github.com>
2023-03-31 07:33:21 +0200

    styling

--- resources/views/layout/general.blade.php
+++ resources/views/layout/general.blade.php
@@ -1,32 +1,41 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>La Rotonde</title>
+    <link rel="stylesheet" href="{{ asset('style.css') }}">
 </head>
 
 <body>
     @guest
-        <div class="navBarNotConnex">
-            <img src="/images/logo.jpg" alt="logo" class="logo">
-            <h1>La Rotonde</h1>
-            <a href="home">Salon</a>
-            <a href="inscription">Inscription</a>
-            <a href="connexion">Connexion</a>
+        <div class="navBar">
+            <div class="left">
+                <img src="{{ asset('logo.png') }}" class="logo" />
+                <h1 class="bigt">La Rotonde</h1>
+            </div>
+            <div class="right">
+                <button><a class="button-link" href="home">Salon</a></button>
+                <button><a class="button-link" href="inscription">Inscription</a></button>
+                <button><a class="button-link" href="connexion">Connexion</a></button>
+            </div>
         </div>
     @endguest
     @auth
-        <div class="navBarConnex">
-            <img src="/images/logo.jpg" alt="logo" class="logo">
-            <h1>La Rotonde</h1>
-            <a href="home">Salon</a>
-            {{-- lister les personnages + creation nouveau --}}
-            <a href="personnages">Personnages</a>
-            <a href="groupes">Groupes</a>
-            <a href="deconnexion">Déconnexion</a>
+        <div class="navBar">
+            <div class="left">
+                <img src="{{ asset('logo.png') }}" alt="logo" class="logo" />
+                <h1 class="bigt">La Rotonde</h1>
+            </div>
+            <div class="right">
+                <button><a class="button-link" href="home">Salon</a></button>
+                {{-- lister les personnages + creation nouveau --}}
+                <button><a class="button-link" href="personnages">Personnages</a></button>
+                <button><a class="button-link" href="groupes">Groupes</a></button>
+                <button><a class="button-link" href="deconnexion">Déconnexion</a></button>
+            </div>
         </div>
     @endauth
 
